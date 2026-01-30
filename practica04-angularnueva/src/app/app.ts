3bdbd121cf2257b097dd7dbc7f27f975f2b212df
ixchelsamperio-hub <ixchelsamperio@micorreo.upp.edu.mx>
2026-01-30 10:14:35 -0600

    practica 5

--- practica04-angularnueva/src/app/app.ts
+++ practica04-angularnueva/src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Navbar } from './components/navbar/navbar';
+import { Footer } from './components/footer/footer';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Navbar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
+  
 })
 export class App {
   protected readonly title = signal('practica04-angularnueva');
