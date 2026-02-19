55588babf8ac0ebd9c868d529439e49389cd7181
ixchelsamperio-hub <ixchelsamperio@micorreo.upp.edu.mx>
2026-02-18 21:12:41 -0600

    practicamodal

--- practica-modal/src/app/app.routes.ts
+++ practica-modal/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Contacto } from './contacto/contacto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Contacto }
+];
+
+
